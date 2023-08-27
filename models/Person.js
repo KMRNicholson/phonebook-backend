fabc557ca882643dbbc9f5fb589a5f79db008df6
Kohdy Nicholson <kohdy.nicholson@varian.com>
2023-08-27 22:57:35 +0300

    feat[3.20]: add validation on the phone number

--- models/Person.js
+++ models/Person.js
@@ -16,7 +16,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: valid => {
+        return /\d{3}-\d{5}/.test(valid) | /\d{2}-\d{6}/.test(valid)
+      },
+      message: props => `${props.value} is not a valid phone number! Valid numbers are in the format 055-5555555 or 05-5555555.`
+    },
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
